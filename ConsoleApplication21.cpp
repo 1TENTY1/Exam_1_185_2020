7a654c24397b70a71180cc9e4efa6b38d6931100
1TENTY1 <71407480+1TENTY1@users.noreply.github.com>
2021-01-22 09:53:25 +0300

    Add files via upload

---
+++ ConsoleApplication21.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string s, output;
+	cin >> s;
+	while (s.size() > 250)
+	{
+		cout << "Input is larger than maximum length of 250\n";
+		cin >> s;
+	}
+	output = s;
+	if (s[0] == '0')
+		output[0] = '0';
+	else output[0] = '1';
+	for (int i = 1; i < s.size(); i++)
+	{
+		if (s[i] == s[i - 1])
+			output[i] = '0';
+		else output[i] = '1';
+	}
+	cout << output << endl;
+}
